f6e631e23a8742a1d99a1f1817d8e1d419f4eb34
Shashikant-15 <shashikant.tanti@knoldus.com>
2023-01-28 20:34:02 +0530

    initial commit

---
+++ src/main/java/com/knoldus/java/BottomViewOfBinaryTree/BottomViewOfBinaryTreeApplication.java
@@ -0,0 +1,13 @@
+package com.knoldus.java.BottomViewOfBinaryTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottomViewOfBinaryTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottomViewOfBinaryTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knoldus/java/BottomViewOfBinaryTree/BottomViewOfBinaryTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knoldus.java.BottomViewOfBinaryTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottomViewOfBinaryTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
